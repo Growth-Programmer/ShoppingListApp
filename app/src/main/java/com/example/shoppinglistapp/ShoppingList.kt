79675bc16bd622ce92bf4d5286e125e3c3924f96
Growth-Programmer <147464744+Growth-Programmer@users.noreply.github.com>
2023-12-01 14:06:35 -0500

    Commit 002

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -3,10 +3,14 @@ package com.example.shoppinglistapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,11 +26,15 @@ data class ShoppingItem(val id:Int,
                         var isEditing: Boolean = false
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 
 fun ShoppingListApp(){
     // Variable that holds the state and list of shop items
     var shopItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember{ mutableStateOf(false)}
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember{ mutableStateOf("") }
     //Column grid that holds all widgets in a column space.
     Column (
         //Modifier is used to fill the entire screen
@@ -35,7 +43,7 @@ fun ShoppingListApp(){
         verticalArrangement = Arrangement.Center)
     {
         Button(
-            onClick = {},
+            onClick = {showDialog=true},
             //Aligns button in the middle of the screen.
             modifier= Modifier.align(Alignment.CenterHorizontally))
         {
@@ -51,4 +59,20 @@ fun ShoppingListApp(){
             }
         }
     }
-}
+
+    if(showDialog){
+        AlertDialog(onDismissRequest = {showDialog=false},
+            confirmButton = { /*TODO*/ },
+            title = {Text("Add Shopping Item")},
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName ,
+                        onValueChange = {itemName = it},
+                        singleLine = true,
+                        modifier= Modifier.fillMaxWidth().padding(8.dp))
+                }
+            }
+        )
+        }
+    }
